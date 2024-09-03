aad099fd6165521ea45e0fc96a962a6eb25fe8b9
Juli-ui <57135670+Juli-ui@users.noreply.github.com>
2024-09-03 03:37:39 -0300

    a

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -37,7 +37,7 @@ public class Checkpoint : MonoBehaviour, IInteractable
 
         _mindPalace.SetActive(palaceActive);
         player.transform.position = position;
-        _ocean.SetActive(palaceActive);
+        //_ocean.SetActive(palaceActive);
         //GameManager.instance.sunLight.gameObject.SetActive(palaceActive);
 
         yield return new WaitForSeconds(1);
