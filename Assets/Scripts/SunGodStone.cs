0396e47cb2b7e11e7e34ca085edb6226b75eb7f3
LuchoMojado <bernacchia4597@gmail.com>
2024-09-29 01:08:55 -0300

    sun god rock wip

--- Assets/Scripts/SunGodStone.cs
+++ Assets/Scripts/SunGodStone.cs
@@ -7,42 +7,51 @@ public class SunGodStone : MonoBehaviour, IInteractable
     [SerializeField] Color _emissiveColor;
     [SerializeField] float _emissiveIntensity;
     [SerializeField] Light _light;
+    [Range(0, 8)]
+    [SerializeField] float _lightIntensity;
+    [SerializeField] float _transitionDuration;
 
     Material _material;
     //bool _toggledOn = false;
 
     private void Start()
     {
-        _material = GetComponent<Material>();
+        _material = GetComponent<Renderer>().material;
     }
 
     public void Interact(PlayerController player)
     {
-        StartCoroutine(ToggleLight(true));
+        StartCoroutine(ToggleLight(true, _transitionDuration));
     }
 
-    IEnumerator ToggleLight(bool on)
+    IEnumerator ToggleLight(bool on, float duration)
     {
-        float timer = 0;
-
+        float timer = 0, lerpT;
+        
         if (on)
         {
-            while (timer < 1)
+            while (timer < duration)
             {
                 timer += Time.deltaTime;
 
-                _material.SetColor("_EmissiveColor", _emissiveColor * Mathf.Lerp(0, _emissiveIntensity, timer));
+                lerpT = timer / duration;
+
+                _material.SetColor("_EmissiveCOlor", _emissiveColor * Mathf.Lerp(0.01f, _emissiveIntensity, lerpT));
+                _light.intensity = Mathf.Lerp(0, _lightIntensity, lerpT);
 
                 yield return null;
             }
         }
         else
         {
-            while (timer < 1)
+            while (timer < duration)
             {
                 timer += Time.deltaTime;
 
-                _material.SetColor("_EmissiveColor", _emissiveColor * Mathf.Lerp(_emissiveIntensity, 0, timer));
+                lerpT = timer / duration;
+
+                _material.SetColor("_EmissiveCOlor", _emissiveColor * Mathf.Lerp(_emissiveIntensity, 0.01f, lerpT));
+                _light.intensity = Mathf.Lerp(_lightIntensity, 0, lerpT);
 
                 yield return null;
             }
