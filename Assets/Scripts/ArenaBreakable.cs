e667cec70114f857b9c793387afdbdfb19e56787
LuchoMojado <bernacchia4597@gmail.com>
2024-10-29 05:34:25 -0300

    oa

--- Assets/Scripts/ArenaBreakable.cs
+++ Assets/Scripts/ArenaBreakable.cs
@@ -6,34 +6,41 @@ using UnityEngine.VFX;
 public class ArenaBreakable : MonoBehaviour, IDamageable
 {
     [SerializeField] float _hp;
+    [SerializeField] VisualEffect _breakVFX;
 
-    VisualEffect _breakVFX;
+    MeshRenderer _renderer;
+    bool _damaged = false;
 
     private void Start()
     {
-        _breakVFX = GetComponent<VisualEffect>();
-        _breakVFX.SetMesh("Mesh", GetComponent<MeshFilter>().mesh);
+        _renderer = GetComponent<MeshRenderer>();
     }
 
     public void TakeDamage(float amount, bool bypassCooldown = false)
     {
         _hp -= amount;
 
-        if (_hp <= 0) StartCoroutine(Death());
+        if (!_damaged && _hp <= 0)
+        {
+            _damaged = true;
+
+            // hacer que parezca medio roto
+        }
+
+        if (_hp <= 0) Die();
     }
 
     IEnumerator Death()
     {
-        _breakVFX.enabled=true;
-        GetComponent<MeshRenderer>().enabled = false;
+        var vfx = Instantiate(_breakVFX, transform.position, transform.rotation);
+        vfx.SetMesh("Mesh", GetComponent<MeshFilter>().mesh);
+        _renderer.enabled = false;
         yield return new WaitForSeconds(1.5f);
-        Die();
+        Destroy(gameObject);
     }
 
     public void Die()
     {
-        
-
-        Destroy(gameObject);
+        StartCoroutine(Death());
     }
 }
