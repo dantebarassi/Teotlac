38149b7cf3c809fc014a49cff633c592cb2830ef
LuchoMojado <bernacchia4597@gmail.com>
2024-10-29 03:54:35 -0300

    breakables vfx

--- Assets/Scripts/ArenaBreakable.cs
+++ Assets/Scripts/ArenaBreakable.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class ArenaBreakable : MonoBehaviour, IDamageable
 {
     [SerializeField] float _hp;
+
+    VisualEffect _breakVFX;
+
+    private void Start()
+    {
+        _breakVFX = GetComponent<VisualEffect>();
+        _breakVFX.SetMesh("Mesh", GetComponent<MeshFilter>().mesh);
+    }
 
     public void TakeDamage(float amount, bool bypassCooldown = false)
     {
@@ -15,6 +24,8 @@ public class ArenaBreakable : MonoBehaviour, IDamageable
 
     public void Die()
     {
+        _breakVFX.Play();
+
         Destroy(gameObject);
     }
 }
